419a97d76a08f47c44f32cf0d9dbb25f0f65c80b
Alvaro Silveira <alvarosilveiraa@gmail.com>
2025-02-23 17:29:57 +0000

    fix

--- android/src/main/java/expo/modules/bluetoothprinter/data/BluetoothPrinterTextOptions.kt
+++ android/src/main/java/expo/modules/bluetoothprinter/data/BluetoothPrinterTextOptions.kt
@@ -3,6 +3,6 @@ package expo.modules.bluetoothprinter.data
 data class BluetoothPrinterTextOptions(
   val align: String? = "left",
   val fontSize: Int? = 1,
-  val isBold: Boolean = false,
-  val isUnderline: Boolean = false,
+  val isBold: Boolean? = false,
+  val isUnderline: Boolean? = false,
 )
