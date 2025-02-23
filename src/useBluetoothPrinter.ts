239691b06fcbf1ba8ab5e5b971a3b9869c9bb99f
Alvaro Silveira <alvarosilveiraa@gmail.com>
2025-02-23 17:57:39 +0000

    test

--- src/useBluetoothPrinter.ts
+++ src/useBluetoothPrinter.ts
@@ -8,31 +8,31 @@ export const useBluetoothPrinter = (deviceName?: string) => {
   const isLoading = useMemo(() => !event, [event]);
   const isEnabled = useMemo(() => BluetoothPrinter.isEnabled(), []);
 
-  const listenDevices = useCallback(async () => {
-    await BluetoothPrinter.checkPermissions();
-    BluetoothPrinter.listenDevices();
-  }, []);
+  // const listenDevices = useCallback(async () => {
+  //   await BluetoothPrinter.checkPermissions();
+  //   BluetoothPrinter.listenDevices();
+  // }, []);
 
-  useEffect(() => {
-    if (!isEnabled) return;
-    listenDevices();
-    return () => {
-      BluetoothPrinter.unlistenDevices();
-    };
-  }, []);
+  // useEffect(() => {
+  //   if (!isEnabled) return;
+  //   listenDevices();
+  //   return () => {
+  //     BluetoothPrinter.unlistenDevices();
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    if (!isEnabled || !devices.length) return;
-    const device = devices.find(
-      ({ name }) => !deviceName || deviceName === name
-    );
-    if (device) {
-      BluetoothPrinter.connectDevice(device.id);
-      return () => {
-        BluetoothPrinter.closeDevice();
-      };
-    }
-  }, [deviceName, devices]);
+  // useEffect(() => {
+  //   if (!isEnabled || !devices.length) return;
+  //   const device = devices.find(
+  //     ({ name }) => !deviceName || deviceName === name
+  //   );
+  //   if (device) {
+  //     BluetoothPrinter.connectDevice(device.id);
+  //     return () => {
+  //       BluetoothPrinter.closeDevice();
+  //     };
+  //   }
+  // }, [deviceName, devices]);
 
   return {
     devices,
